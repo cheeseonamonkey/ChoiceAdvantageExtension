c8f7b689d6a45563d5eae4314bf3922493a14a4b
Alexander H <54555500+cheeseonamonkey@users.noreply.github.com>
2025-09-21 21:33:17 -0600

    Update highlightDnrs.js

--- highlightDnrs.js
+++ highlightDnrs.js
@@ -15,8 +15,11 @@ function highlightDNRs() {
 
       if (text.includes(last) && text.includes(first)) {
         link.innerHTML = `
-          <span class="hover_show" style="padding: 1px; text-shadow 1px 1px 1x rgba(11,11,11,0.35); padding: 1px; background-color: rgba(00,33,33, 0.08); font-weight: bold; line-height: 0.5; position: relative; top:4px; left:-2px; font-family: Roboto; font-size: 0.95em; color:#f44;"><i>Check DNR</i></span><br/>
-          <span style="text-decoration: underline #f44; text-decoration-thickness:1.5px;">${link.innerText}</span>
+          
+          <span style="text-decoration: underline #f44; text-decoration-thickness:1.5px;">
+		  <span class="hover_show" style="padding: 1px; text-shadow 1px 1px 1x rgba(11,11,11,0.37); padding: 1px; background-color: rgba(22,44,99, 0.85); margin-top: 0.2em; font-weight: bold; position: absolute; font-family: Roboto; font-size: 0.95em; color:#f44;"><i>Check DNR</i><br/></span>
+		  ${link.innerText}
+		  </span>
         `;
       }
     });
