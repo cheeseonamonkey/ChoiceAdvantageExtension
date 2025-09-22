052137a66d890269b759bed79241fadcd0a26338
Alexander H <54555500+cheeseonamonkey@users.noreply.github.com>
2025-09-21 19:41:40 -0600

    Create highlightDnrs.js

---
+++ highlightDnrs.js
@@ -0,0 +1,23 @@
+const dnrList = ["Doe, John", "Huso, Alexander", "POTTER, HARRY" ]  // add your own names here as: First, Last
+
+
+function highlightDNRs() {
+  const links = document.querySelectorAll('table a');
+
+  links.forEach(link => {
+    const text = link.innerText.toUpperCase();
+
+    dnrList.forEach(name => {
+      const [last, first] = name.toUpperCase().split(',').map(s => s.trim()); 
+
+      if (text.includes(last) && text.includes(first)) {
+        link.innerHTML = `
+          <span style="font-weight:bold; line-height: 0.5; position: relative; top:4px; left:-1px; font-family: Roboto; font-size: 0.67em; color:red;"><i>Check DNR</i></span><br/>
+          <span style="text-decoration: underline red; text-decoration-thickness:1.2px;">${link.innerText}</span>
+        `;
+      }
+    });
+  });
+}
+
+highlightDNRs();
